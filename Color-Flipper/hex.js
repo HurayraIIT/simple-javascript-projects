ba01cfc97d3673f5561ab76682f93bcb2d43a004
Abu Hurayra <hurayraiit@gmail.com>
2022-01-16 19:39:30 +0600

    Added Project: Color Flipper

--- Color-Flipper/hex.js
+++ Color-Flipper/hex.js
@@ -1,1 +1,22 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function(){
+    const randomColor = getRandomColor();
+    document.body.style.backgroundColor = randomColor;
+    color.textContent = randomColor;
+});
+
+function getRandomColor() {
+    let color = "#";
+    for(let i=0;i<6;i++) {
+        color += hex[getRandomNumber()];
+    }
+    return color;
+}
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * hex.length);
+}
